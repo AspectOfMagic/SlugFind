a5f1b9316769be1d941b8bd19bab476ac584478e
Hasan Turkoz <hasan@Hasans-MacBook-Air.local>
2024-05-14 16:45:35 -0700

    adding marker switch

--- slug_find_app/lib/main.dart
+++ slug_find_app/lib/main.dart
@@ -6,7 +6,6 @@ import 'firebase_options.dart';
 // TODO(codelab user): Get API key
 const clientId = 'YOUR_CLIENT_ID';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
